f7f12375dcc5a59d60f0f92168ec59f6901c203e
David Haylock <david.haylock@watershed.co.uk>
2015-09-01 23:29:28 +0100

    - open file read specific length

--- Infrequency.py
+++ Infrequency.py
@@ -9,19 +9,12 @@ import httplib,urllib
 import json
 import os
 import sys
-
+import subprocess
 import soundcloud
-
 
 M_PI = 3.141592
 
-clientID = os.system("cat id.txt")
-# create a client object with your app credentials
-client = soundcloud.Client(client_id=clientID)
-streamURLClient = client
-
-print clientID
-
+#----------------------------------------------------
 # Function to return the degrees from the Accelorometer
 def convertAccelToAngle(x,y,z):
     roll = (math.atan2(-y,z)*180)/M_PI
@@ -30,13 +23,32 @@ def convertAccelToAngle(x,y,z):
     print roll
     print pitch
 
+#----------------------------------------------------
+# Setup the stream
+def setup():
+    fileOpen = open("id.txt",'r+')
+    clientID = fileOpen.read(32);
+    fileOpen.close()
 
+    print '------------------------'
+    print 'Client ID'
+    print clientID
+
+    client = soundcloud.Client(client_id=clientID)
+    streamURLClient = client
+    print '------------------------'
+    print 'Getting tracklist'
+    try:
+        tracks = client.get('/tracks', q='bbc')
+    except Exception, e:
+        print 'Error: %s, Status Code: %d' % (e.message, e.response.status_code)
+
+    for track in tracks:
+        newURL = streamURLClient.get(track.stream_url,allow_redirects=False)
+        print '------------------------'
+        print track.title
+        print ""
+        print newURL.location
 
 # convertAccelToAngle(0.0,0.0,0.0)
-
-
-tracks = client.get('/tracks', q='bbc')
-
-for track in tracks:
-    newURL = streamURLClient.get(track.stream_url,allow_redirects=False)
-    print newURL.location
+setup()
