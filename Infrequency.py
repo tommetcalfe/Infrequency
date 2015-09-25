d74c796f7258b9687377559505d92f53841130ad
David Haylock <david.haylock@watershed.co.uk>
2015-09-25 22:38:01 +0100

    - sound cloud search

--- Infrequency.py
+++ Infrequency.py
@@ -1,7 +1,7 @@
-#!/usr/bin/env python
+#!/usr/bin/env python 
 import string
-import random
-from random import shuffle
+#import random
+#from random import shuffle
 import sys
 import datetime
 import math
@@ -10,33 +10,51 @@ import json
 import os
 import sys
 
-import soundcloud
+import urllib2
+contents = urllib2.urlopen("https://itunes.apple.com/search?term=gardening&entity=podcast").read()
+
+print contents
+
+#
+#import soundcloud
+#
+#
+#M_PI = 3.141592
+#
+#clientID = os.system("cat id.txt")
+## create a client object with your app credentials
+#client = soundcloud.Client(client_id="fe503f838868c2301b391f1877716edd")
+##streamURLClient = client
+#
+#print clientID
+#
+## Function to return the degrees from the Accelorometer
+#def convertAccelToAngle(x,y,z):
+#    roll = (math.atan2(-y,z)*180)/M_PI
+#    pitch = (math.atan2(x,math.sqrt(y*y + z*z))*180)/M_PI
+#    # yaw = (math.atan(math.sqrt(x)+math.sqrt(y)/z)*180)/M_PI
+#    print roll
+#    print pitch
+#
+#
+#
+## convertAccelToAngle(0.0,0.0,0.0)
 
 
-M_PI = 3.141592
 
-clientID = os.system("cat id.txt")
-# create a client object with your app credentials
-client = soundcloud.Client(client_id=clientID)
-streamURLClient = client
-
-print clientID
-
-# Function to return the degrees from the Accelorometer
-def convertAccelToAngle(x,y,z):
-    roll = (math.atan2(-y,z)*180)/M_PI
-    pitch = (math.atan2(x,math.sqrt(y*y + z*z))*180)/M_PI
-    # yaw = (math.atan(math.sqrt(x)+math.sqrt(y)/z)*180)/M_PI
-    print roll
-    print pitch
-
-
-
-# convertAccelToAngle(0.0,0.0,0.0)
-
-
-tracks = client.get('/tracks', q='bbc')
-
-for track in tracks:
-    newURL = streamURLClient.get(track.stream_url,allow_redirects=False)
-    print newURL.location
+#
+#import soundcloud
+#
+## create a client object with your app credentials
+#client = soundcloud.Client(client_id='fe503f838868c2301b391f1877716edd')
+#
+## find all sounds of buskers licensed under 'creative commons share alike'
+#tracks = client.get('/tracks',query='gardening')
+#
+#for track in tracks:
+#    newURL = client.get(track.stream_url,allow_redirects=False)
+#    print "------------------------"
+#    print track.title
+#    print newURL.location
+#
+#print "------------------------"
